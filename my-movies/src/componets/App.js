6950a5ddc6093dd1d5e33cde062c6083d7ec90db
Graudss <yusuf.guner.766@gmail.com>
2023-08-23 17:14:09 +0300

    update

--- my-movies/src/componets/App.js
+++ my-movies/src/componets/App.js
@@ -1,9 +1,44 @@
 import React from 'react';
-
+import SearchBar from './SearchBar';
+import MoveList from './MoveList';
 class App extends React.Component {
+
+    state = {
+        movies: [{
+            "id": 1,
+            "name": "The Flash",
+            "rating": 8.3,
+            "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
+            "imageURL": "https://img.freepik.com/free-psd/music-shop-poster-template_23-2148855476.jpg?w=826&t=st=1692783640~exp=1692784240~hmac=7aa1c0b4360e433bb7314b5e1cb721059499b9fcff470ed071c517083d1670e1"
+        }, {
+            "id": 2,
+            "name": "Interstellar",
+            "rating": 6.8,
+            "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
+            "imageURL": "https://img.freepik.com/free-psd/black-lives-matter-poster-template_23-2148585425.jpg?w=826&t=st=1692783679~exp=1692784279~hmac=552370f0e6947c69f1431a880114415c9d73b7c9c5b436564f89b7d491370701"
+        }, {
+
+            "id": 3,
+            "name": "Arrow",
+            "rating": 7.9,
+            "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
+            "imageURL": "https://img.freepik.com/premium-psd/poster-design-photo-manipulation-template_528542-1009.jpg?w=740"
+        }
+        ]
+    }
+
     render() {
+
         return (
-            <h1> Helloo My Movies</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-1g-12">
+                        <SearchBar />
+                    </div>
+                </div>
+                <MoveList 
+                    movies = {this.state.movies} />
+            </div>
         )
 
     }
---
+++ my-movies/src/componets/MoveList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const MoveList = (props) => {
+    function handleClick(event){
+        console.log(event.pageX);
+        //alert("OOOOOOOOOOOOOOOOOOOOOOOOO");
+    }
+
+    return (
+        <div className="row">
+            {props.movies.map((movie) => (
+                <div className="col-lg-4" key={movie.id}>
+                    <div className="card mb-4 shadow-sm">
+                        <img src={movie.imageURL} className="card-img-top" alt="Sample Movie"></img>
+                        <div className="card-body">
+                            <h5 className="card-title">{movie.name}</h5>
+                            <p className='card-text'>{movie.overview}</p>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <button onClick={handleClick} type="button" className="btn btn-outline-danger" ></button>
+                                <h2><span className="badge bg-info">{movie.raitng}</span></h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+
+
+
+
+
+        </div>
+    )
+
+}
+
+export default MoveList;
